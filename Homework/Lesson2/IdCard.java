7a518d3733598f356323dd9e116d80c706a5578d
ipalis <ivanpalis@gmail.com>
2016-04-17 15:54:03 +0300

    added to homework2 some files with classes

---
+++ Homework/Lesson2/IdCard.java
@@ -0,0 +1,26 @@
+public class IdCard extends WorkerOfCompany {
+			private int cardNumber;
+			private date dateExpire;
+	
+public IdCard(){
+		this.cardNumber = cardNumber;
+		this.dateExpire = dateExpire;
+		}
+
+public void getCardNumber() {
+		return cardNumber;
+		}
+
+public void setCardNumber(int id){
+		this.cardNumber = cardNumber;
+		}
+
+public void getDateExpire() {
+		return dateExpire;
+		}
+
+public void setDateExpire(date dateExpire){
+		this.dateExpire = dateExpire;
+		}
+}
+
---
+++ Homework/Lesson2/Violin.java
@@ -0,0 +1,28 @@
+public class Violin extends MusicInstruments {
+			private double price;
+			private String model;
+			private boolean isCarryingCase;
+			private boolean isNotes;
+			private String producer;
+				
+	
+public Violin (){
+		this.price = price;
+		this.model = model;
+		this.isCarryingCase = isCarryingCase;
+		this.isNotes = isNotes;
+		this.producer = producer;
+		}
+
+public String getViolin() {
+		return type;
+}
+public void  playTheNotes(){
+		
+}
+
+public void  playWithOrchestra(){
+		
+}
+}
+
