6beb4ba1c9dcbaa788d105dae7b36eee926d3acb
ipalis <ivanpalis@gmail.com>
2016-07-06 01:31:27 +0300

    added test project

---
+++ MyFirstProject/src/main/java/myApplication/App.java
@@ -0,0 +1,13 @@
+package myApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
